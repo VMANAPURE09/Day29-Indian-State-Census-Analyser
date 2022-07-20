1738a21c646e21c0cf6d6633a79c6a131816d6e1
tutorial6 <manapure.vaibhav542@gmail.com>
2022-07-20 18:36:24 +0530

    TC-1.2: If incorrect Returns custom Exception

--- CensusAnalyserTest/UnitTest1.cs
+++ CensusAnalyserTest/UnitTest1.cs
@@ -9,20 +9,39 @@ namespace CensusAnalyserTest
 {
     public class Tests
     {
-       
+
+        static string indianStateCensusHeaders = "State,Population,AreaInSqKm,DensityPerSqKm";
+
+        static string indianStateCensusFilePath = @"E:\BridgeLabz2\PraticeProblem\Day29-Indian-State-Census-Analyser\CensusAnalyserTest\CSVFiles\IndiaStateCensusData.csv";
+        static string wrongIndianStateCensusFilePath = @"E:\BridgeLabz2\PraticeProblem\Day29-Indian-State-Census-Analyser\CensusAnalyserTest\CSVFiles\IndiaData.csv";
+
+        IndianStatesCensusAnalyser.CensusAnalyser censusAnalyser;
+        Dictionary<string, CensusDTO> totalRecord;
+        Dictionary<string, CensusDTO> stateRecord;
         [SetUp]
         public void Setup()
         {
-           
+            censusAnalyser = new IndianStatesCensusAnalyser.CensusAnalyser();
+            totalRecord = new Dictionary<string, CensusDTO>();
+            stateRecord = new Dictionary<string, CensusDTO>();
         }
 
-   
        
+        //TC 1.1 - Check to ensure the Number of Record matches
         [Test]
-        public void Test1()
+        public void GivenIndianCensusDataFile_WhenReaded_ShouldReturnCensusDataCount()
         {
-            Assert.Pass();
+            totalRecord = censusAnalyser.LoadCensusData(CensusAnalyser.Country.INDIA, indianStateCensusFilePath, indianStateCensusHeaders);
+            Assert.AreEqual(5, totalRecord.Count);
         }
 
+        //Wrong File Path
+        //Tc 1.2
+        [Test]
+        public void GivenWrongIndianCensusCodeFilePath_WhenRead_ShouldReturn_FILE_NOT_FOUND()
+        {
+            var censusException = Assert.Throws<CensusAnalyserException>(() => censusAnalyser.LoadCensusData(CensusAnalyser.Country.INDIA, wrongIndianStateCensusFilePath, indianStateCensusHeaders));
+            Assert.AreEqual(CensusAnalyserException.ExceptionType.FILE_NOT_FOUND, censusException.eType);
+        }
     }
 }
