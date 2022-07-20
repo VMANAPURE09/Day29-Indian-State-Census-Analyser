25cc88e3237ce80a01543939510a45f448d70388
tutorial6 <manapure.vaibhav542@gmail.com>
2022-07-20 17:48:39 +0530

    Load the Indian States CEnsus Information from a csv file

---
+++ CensusAnalyserTest/UnitTest1.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+using IndianStatesCensusAnalyser;
+using IndianStatesCensusAnalyser.POCO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using static IndianStatesCensusAnalyser.CensusAnalyser;
+
+namespace CensusAnalyserTest
+{
+    public class Tests
+    {
+       
+
+        [SetUp]
+        public void Setup()
+        {
+           
+        }
+        
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+       
+    }
+}
---
+++ IndianStatesCensusAnalyser/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Twilio;
+using Twilio.Rest.Verify.V2.Service;
+
+namespace IndianStatesCensusAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
