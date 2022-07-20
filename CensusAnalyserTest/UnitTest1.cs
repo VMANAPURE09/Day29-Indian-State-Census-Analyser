5b2684d9255f2528737b132914f7c16d2560597b
tutorial6 <manapure.vaibhav542@gmail.com>
2022-07-20 18:20:00 +0530

    TC-1.1: Check to Ensure the Number of Record matches

--- CensusAnalyserTest/UnitTest1.cs
+++ CensusAnalyserTest/UnitTest1.cs
@@ -9,19 +9,30 @@ namespace CensusAnalyserTest
 {
     public class Tests
     {
-       
 
+        static string indianStateCensusHeaders = "State,Population,AreaInSqKm,DensityPerSqKm";
+
+        static string indianStateCensusFilePath = @"E:\LFP158\Assignment\Day 29\IndianStateCensusAnalyserProblem\IndianStateCensusAnalyserProblem\CSV\IndiaStateCensusData.csv";
+
+        IndianStatesCensusAnalyser.CensusAnalyser censusAnalyser;
+        Dictionary<string, CensusDTO> totalRecord;
+        Dictionary<string, CensusDTO> stateRecord;
         [SetUp]
         public void Setup()
         {
-           
+            censusAnalyser = new IndianStatesCensusAnalyser.CensusAnalyser();
+            totalRecord = new Dictionary<string, CensusDTO>();
+            stateRecord = new Dictionary<string, CensusDTO>();
         }
-        
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
+
        
+        //TC 1.1 - Check to ensure the Number of Record matches
+        [Test]
+        public void GivenIndianCensusDataFile_WhenReaded_ShouldReturnCensusDataCount()
+        {
+            totalRecord = censusAnalyser.LoadCensusData(CensusAnalyser.Country.INDIA, indianStateCensusFilePath, indianStateCensusHeaders);
+            Assert.AreEqual(5, totalRecord.Count);
+        }
+
     }
 }
